e878964383498b1f58c3007575e44481d0227d07
huangjiaxing <huangjiaxing@aspirecn.com>
2026-01-06 13:34:30 +0800

    build(api): 升级 Wrangler 至 v4 并统一 Drizzle ORM 版本

---
+++ commitlint.config.mjs
@@ -0,0 +1,25 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [0, 'never'],
+  },
+}
