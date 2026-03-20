97b876a3e15888d54e7c8f28260b96e7280da70f
huangjiaxing <huangjiaxing@aspirecn.com>
2026-03-21 01:02:20 +0800

    fix(ci): :bug: 修复ci部署问题

--- packages/db/drizzle/0017_puzzling_war_machine.sql
+++ packages/db/drizzle/0017_puzzling_war_machine.sql
@@ -1,4 +1,4 @@
-DROP INDEX `idx_movie_actor_actor_id`;--> statement-breakpoint
-CREATE INDEX `idx_movie_actor_actor_id` ON `movie_actor` (`actor_id`);--> statement-breakpoint
-DROP INDEX `idx_movie_pub_publisher_id`;--> statement-breakpoint
-CREATE INDEX `idx_movie_pub_publisher_id` ON `movie_publisher` (`publisher_id`);
+DROP INDEX IF EXISTS `idx_movie_actor_actor_id`;--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS `idx_movie_actor_actor_id` ON `movie_actor` (`actor_id`);--> statement-breakpoint
+DROP INDEX IF EXISTS `idx_movie_pub_publisher_id`;--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS `idx_movie_pub_publisher_id` ON `movie_publisher` (`publisher_id`);
